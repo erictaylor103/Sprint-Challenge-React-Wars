b63dbf92d46b00def9ff379c27532e3594ed44e2
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-04-17 13:56:38 -0400

    Added styling to the MainContainer and Character container. Also completed the axios call. Met MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import MainContainer from './components/MainContainer.js';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,21 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("https://swapi.py4e.com/api/people")
+    .then(response => {
+      //console.log(response.data.results);
+      setData(response.data.results)
+    }).catch(error => {
+      //console.log('Error', error);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      <MainContainer data = {data}/>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+
+import React from 'react';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+    width: 18%;    
+    margin-top:45px;
+    background-color:white; 
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 10), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    border: 10px solid #cd854c;
+    background: rgba(255, 255, 255, 0.5);
+    border-radius: 10% 30% 50% 70%;
+    padding: 10px;
+
+`;
+
+
+const Character = props =>{ //passed the characters data via "props" from the data variable in App.js
+    //console.log(props);
+    
+    return( //return the star wars character's data we collected via props
+        <CardContainer>
+            <h2>Name:       {props.charData.name}</h2>
+                DOB:        {props.charData.birth_year}<br/><br/>
+                Sex:        {props.charData.gender}<br/><br/>
+                Height:     {props.charData.height}cm<br/><br/>                                            
+        </CardContainer>
+    )
+}
+export default Character;
+
